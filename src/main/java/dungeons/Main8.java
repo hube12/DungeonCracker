d3444933198fb98276179d9af845575405ab83ac
Neil <H66r10@gmail.com>
2020-05-01 23:46:57 +0200

    adapted

--- src/main/java/dungeons/Main8.java
+++ src/main/java/dungeons/Main8.java
@@ -30,26 +30,26 @@ public class Main8 {
 
     public static void main(String[] args) throws InterruptedException {
         //============================================================ START INPUT
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter number of threads");
-        int threads = in.nextInt();
-        int THREAD_BITS = highestPowerof2(threads);
-        System.out.println("Enter posX of spawner");
-        int posX = in.nextInt();
-        System.out.println("Enter posY of spawner");
-        int posY = in.nextInt();
-        System.out.println("Enter posZ of spawner ");
-        int posZ = in.nextInt();
-        System.out.println("Enter the sequence Read it from the image with the supplied script");
-        String stringPattern = in.nextLine();
-        stringPattern = in.nextLine();
+        //Scanner in = new Scanner(System.in);
+        //System.out.println("Enter number of threads");
+        //int threads = in.nextInt();
+        int THREAD_BITS = highestPowerof2(8);
+        //System.out.println("Enter posX of spawner");
+        //int posX = in.nextInt();
+        //System.out.println("Enter posY of spawner");
+        //int posY = in.nextInt();
+        //System.out.println("Enter posZ of spawner ");
+        //int posZ = in.nextInt();
+        //System.out.println("Enter the sequence Read it from the image with the supplied script");
+        //String stringPattern = in.nextLine();
+        //stringPattern = in.nextLine();
         int THREAD_COUNT = 1 << THREAD_BITS;
         System.out.println("Running on " + THREAD_COUNT + " threads");
         ExecutorService SERVICE = Executors.newFixedThreadPool(THREAD_COUNT);
-        //int posX = -38;
-        //int posY = 26;
-        //int posZ = 127;
-        //String stringPattern = "101110111001111111111111110110111111111111110111110110010011111";
+        int posX = 402 ;
+        int posY = 50;
+        int posZ = -146;
+        String stringPattern = "010101001101110110111100001101111011111101111111111111111101010111111110111111111";
         // 1 mossy
         // 0 cobble
         //============================================================ END INPUT
@@ -62,28 +62,37 @@ public class Main8 {
         LCG skipFloorSize = Rand.JAVA_LCG.combine(3);
 
         List<Long> decoratorSeeds = new ArrayList<>();
-        System.out.format("Seed space is in the range [%d, %d). \n", (long) posY << 40, (long) (posY + 1) << 40);
+        System.out.format("Seed space is in the range [%d, %d). \n", (long) posY << 39, (long) (posY + 1) << 39);
         AtomicInteger completedThreads = new AtomicInteger(0);
 
         for (int threadId = 0; threadId < THREAD_COUNT; threadId++) {
-            long seedsPerThread = (1L << 40) >> THREAD_BITS;
-            long lower = ((long) posY << 40) + seedsPerThread * threadId;
+            long seedsPerThread = (1L << 39) >> THREAD_BITS;
+            long lower = ((long) posY << 39) + seedsPerThread * threadId;
             long upper = lower + seedsPerThread;
             System.out.format("Thread %d starting with bounds [%d, %d). \n", threadId, lower, upper);
             SERVICE.submit(() -> {
                 for (long seed = lower; seed < upper; seed++) {
+
+                    /*-
+                    int k2 = i + rand.nextInt(16) + 8; <- back (-1)
+                    int k3 = rand.nextInt(128); <- seed
+                    int i4 = j + rand.nextInt(16) + 8; <-  next(1)
+                    int i = par2Random.nextInt(2) + 2;  <- skip(1)
+                    int j = par2Random.nextInt(2) + 2;  <- skip(1)
+
+                     */
                     long seedCopy = seed;
                     long temp = back.nextSeed(seed);
                     if (temp >>> (48 - 4) != offsetX) continue;
-                    temp = Rand.JAVA_LCG.nextSeed(seed);
+                    temp = Rand.JAVA_LCG.nextSeed(seed); // back(-1)
 
                     if (temp >>> (48 - 4) != offsetZ) continue;
-                    seedCopy = skipFloorSize.nextSeed(seedCopy);
+                    seedCopy = skipFloorSize.nextSeed(seedCopy); // next(1)
 
                     boolean floorMatches = true;
                     int count=0;
                     for (int block : pattern) {
-                        seedCopy = Rand.JAVA_LCG.nextSeed(seedCopy);
+                        seedCopy = Rand.JAVA_LCG.nextSeed(seedCopy); // next(1)+skip(2)
                         int nextInt4 = (int) (seedCopy >>> (48 - 2));
                         // goes from -x to +x
                         // and -z to +z
