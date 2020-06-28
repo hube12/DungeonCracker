5991d8f622af0c4f526cc46772d4d83a6684f057
Neil <h66r10@gmail.com>
2020-06-28 23:01:36 +0200

    fix ordinal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,15 @@ import kaptainwutax.biomeutils.Biome;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new VersionCrack(MCVersion.vLegacy,-433,17,-1162,
-                "110111122"+
-                        "101100112"+
-                        "211011112"+
-                        "211110112"+
-                        "001101100"+
-                        "111111111"+
-                        "111110111").run().toString());
+
+       }
+       public static void test1_16(){
+           // -720350949281663006
+        System.out.println(new VersionCrack(MCVersion.v1_16,-1840,23,-1275,"1001011111001010111011100110111110101001011101011",Biome.OCEAN).run().toString());
+        System.out.println(new VersionCrack(MCVersion.v1_16,-2420,40,-1693,"1111101100100111101111000011110101110101100111111",Biome.JUNGLE_EDGE).run().toString());
+
+        System.out.println(new VersionCrack(MCVersion.v1_16,-5620,34,-2062,"111110111101111111100101111111110110011111110111101111111001211011101111001101011",Biome.SWAMP).run().toString());
+        System.out.println(new VersionCrack(MCVersion.v1_16,-6799,61,-1473,"011010011111011111011110011100111011110111011110001011110111011111000011111101011",Biome.DESERT).run().toString());
+
     }
 }
